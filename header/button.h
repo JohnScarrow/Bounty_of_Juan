7dcc5eeee5cc3857f88c3e3467352d531fbfb75b
Zipline25 <tan080706@gmail.com>
2026-04-14 03:53:25 +0000

    added pretty much everything for the button class to function, look at the bottom function of button.cpp to use it

--- header/button.h
+++ header/button.h
@@ -1,6 +1,23 @@
 /**
- * @file button.h
- * @author your name (you@domain.com)
- * @brief makes and handles button input, what all to do when a button is pressed.
- * @date 2026-04-07
- */
+* @file button.h
+* @author Nathan, Brayden, Tristan, John
+* @brief makes and handles button input, what all to do when a button is pressed.
+* @date 2026-04-13
+*/
+#ifndef BUTTON_H
+#define BUTTON_H
+#include <SFML/Graphics.hpp>
+
+class Button{
+public:
+    Button(sf::Vector2f size, sf::Vector2f position, sf::Font& font, const std::string& text);
+    void render(sf::RenderWindow& window);
+    void update(const sf::RenderWindow& window);
+    bool buttonClicked();
+private:
+    sf::RectangleShape mShape;
+    sf::Text mText;
+    bool mClicked = false;
+};
+
+#endif
