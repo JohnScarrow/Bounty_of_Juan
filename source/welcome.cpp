f6317ddfa5124058d9462982bbe4eaef8298a58a
JohnScarrow <JohnA@Scarrows.com>
2026-04-24 20:03:17 -0700

    Main fixed and running welcome screen with button. Also added .gitignore entry for game folder to avoid accidentally committing binaries.

--- source/welcome.cpp
+++ source/welcome.cpp
@@ -1,51 +1,46 @@
 #include "../header/welcome.h"
+#include <iostream>
 
 Welcome::Welcome()
 {
     if (!mFont.loadFromFile("assets/westernFonts.otf"))
     {
-        std::cout<<"Error opening file\n";
+        std::cout << "Error opening file\n";
         exit(2);
     }
-    
-    mHeader.setFont(mFont);
-    //choose the font size based on button size (I choose half)
-    mHeader.setCharacterSize(20);
 
-    // //set label
+    mHeader.setFont(mFont);
+    mHeader.setCharacterSize(20);
     mHeader.setString("Bounty of Juan");
 
     mRules.setFont(mFont);
-    //choose the font size based on button size (I choose half)
     mRules.setCharacterSize(16);
-
-    // //set label
     mRules.setString("Start\n");
     mRules.setPosition({10, 30});
 
-    // mStart.setText("Start");
-    // mStart.setPosition({580, 450});
-    // mStart.setSize({60, 20});
-    mStart.setColorTextNormal(sf::Color::Blue);
-    //mStart.setColor(sf::Color(255,255,0));
+    mStart = std::make_unique<Button>(
+        sf::Vector2f(120, 40),
+        sf::Vector2f(440, 380),
+        mFont,
+        "Start"
+    );
 }
 
 State Welcome::handleInput(sf::Event& e, sf::RenderWindow& window)
 {
-    if (mStart.handleInput(e, window)){
+    if (mStart->buttonClicked())
         return game;
-    }
     return welcome;
 }
 
-void Welcome::update()
+void Welcome::update(const sf::RenderWindow& window)
 {
-    mStart.update();
+    mStart->update(window);
 }
 
 void Welcome::render(sf::RenderWindow& window)
 {
     window.draw(mHeader);
     window.draw(mRules);
-    window.draw(mStart);
+    mStart->render(window);
 }
